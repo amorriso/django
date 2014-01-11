149a6a22b33b2c66030d06472eb6224acce5940c
TonyM (windows) <anthony.e.morrison@gmail.com>
2014-01-11 05:48:23 +0000

    fixing Straddle spelling mistake. Stradle should be Straddle

--- trader/trader/settings.py
+++ trader/trader/settings.py
@@ -19,7 +19,7 @@ LOGIN_URL = "/admin"
 sys.path.append(DJANGO_ROOT)
 ########## END PATH CONFIGURATION
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -44,7 +44,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
