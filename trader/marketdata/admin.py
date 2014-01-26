868a17d84700396ee9fad51bc665a8556d412630
TonyM (windows) <anthony.e.morrison@gmail.com>
2014-01-26 15:43:41 +0000

    adding expiry date field to futures admin. This fixes bug where new futures were not showing up (they were being automatically populated with expiry dates 1900-01-01, which is obviously in the past and so these were not showing up in the marketdata index page

--- trader/marketdata/admin.py
+++ trader/marketdata/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from marketdata.models import *
 
 class FutureAdmin(admin.ModelAdmin):
-    fields = ['name', 'bloomberg_id', 'easyscreen_id', 'month_tag']
+    fields = ['name', 'bloomberg_id', 'easyscreen_id', 'month_tag', 'expiry_date']
 
 #class OptionDefinitionAdmin(admin.ModelAdmin):
 #    fields = ['name', 'bloomberg_prefix', 'easyscreen_prefix', 'month_tag', 'expiry_date', 'strike_interval', 'price_movement', 'number_of_OTM_options']
