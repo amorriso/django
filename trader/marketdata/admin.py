7ca48631c07df8b26324b4cba0aced68e0e5f860
TonyM (windows) <anthony.e.morrison@gmail.com>
2014-02-04 17:58:28 +0000

    removing price movement from add option definition. this is not needed

--- trader/marketdata/admin.py
+++ trader/marketdata/admin.py
@@ -8,7 +8,8 @@ class FutureAdmin(admin.ModelAdmin):
 
 class OptionDefinitionAdmin(admin.ModelAdmin):
     #fields = ['name', 'future', 'easyscreen_prefix', 'month_tag', 'expiry_date', 'strike_interval', 'price_movement', 'number_of_OTM_options']
-    fields = ['name', 'future', 'easyscreen_prefix', 'expiry_date', 'strike_interval', 'price_movement', 'number_of_OTM_options']
+    #fields = ['name', 'future', 'easyscreen_prefix', 'expiry_date', 'strike_interval', 'price_movement', 'number_of_OTM_options']
+    fields = ['name', 'future', 'easyscreen_prefix', 'expiry_date', 'strike_interval', 'number_of_OTM_options']
 
 admin.site.register(Future, FutureAdmin)
 admin.site.register(OptionDefinition, OptionDefinitionAdmin)
